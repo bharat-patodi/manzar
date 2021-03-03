ba48be1296f413697f5fb47bbd287787b96ad560
tinkalguli <tinkalmunu@gmail.com>
2021-03-03 21:41:12 +0530

    Add path variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
