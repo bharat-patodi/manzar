fd798de883d15a27b8cae21077344f8b054e1d6f
vishakha011 <khatadevishakha01@gmail.com>
2021-03-08 09:52:13 +0530

    add google oauth button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -30,7 +30,7 @@ class Dashboard extends React.Component {
     return (
       <section className="dashboard">
         <div className="dashboard__card-showcase">
-          <Card
+          <Card 
             prominentImage="https://images.unsplash.com/photo-1603539947678-cd3954ed515d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
             title="Getify Portfolio"
             author={this.state.portfolios[1].author.username}
